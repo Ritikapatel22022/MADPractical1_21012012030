247397ff0dcc24c4a003f86a8b5569b1a74fe8ca
yashh111 <lakhaniyash111@gmail.com>
2022-08-06 15:12:06 +0530

    practical1.1

--- src/main/kotlin/practical1_1.kt
+++ src/main/kotlin/practical1_1.kt
@@ -1,5 +1,22 @@
 fun main(){
     var a:Int=12344
+    var b:Double=20.3
+    var c:Float=1.1234F
+    var d:Long=1000000
+    var e:Short=320
+    var f:Byte=127
+    var g:Char='R'
+    var h:Boolean=true
+    var i:String="Ritika"
+    println("21012012030\n")
     println("integer value:$a")
+    println("double value:$b")
+    println("float value:$c")
+    println("long value:$d")
+    println("short value:$e")
+    println("byte value:$f")
+    println("character value:$g")
+    println("booloean value:$h")
+    println("string value:$i")
 
 }
